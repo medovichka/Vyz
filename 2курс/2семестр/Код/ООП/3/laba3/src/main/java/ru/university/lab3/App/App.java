bc64eaf47d8b9a9db59a0e6fee84a3d6b46e0797
Medovichka <killopolop03@gmail.com>
2026-04-13 11:24:04 +0300

    3 лаба по ООП, конспекты по оптимизации

--- 2курс/2семестр/Код/ООП/3/laba3/src/main/java/ru/university/lab3/App/App.java
+++ 2курс/2семестр/Код/ООП/3/laba3/src/main/java/ru/university/lab3/App/App.java
@@ -2,9 +2,12 @@ package ru.university.lab3.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-// ДОБАВЛЕНА НАСТРОЙКА СКАНИРОВАНИЯ!
 @SpringBootApplication(scanBasePackages = "ru.university.lab3")
+@EntityScan(basePackages = "ru.university.lab3.entity")
+@EnableJpaRepositories(basePackages = "ru.university.lab3.repository")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
