1897fa99ee8b6f48467569b5592dec9feb623175
medovichka <killopolop03@gmail.com>
2026-06-27 23:03:29 +0300

    Конец 2 курса

--- 2курс/2семестр/Код/ООП/3/laba3/src/main/java/ru/university/lab3/repository/EmployeesRep.java
+++
@@ -1,9 +0,0 @@
-package ru.university.lab3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.university.lab3.entity.Employees;
-
-@Repository
-public interface EmployeesRep extends JpaRepository<Employees, Integer> {
-}
